ba97356fca0ab03694b8ba8193c227427c415ef8
aniketanshuman <aniket@iiitmanipur.ac.in>
2023-06-24 12:55:47 +0530

    Initial Commit

---
+++ src/Components/Footer/Footer2.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer2 = () => {
+  return (
+    <div className="footer2">
+      <hr className="break" />
+      <p className="coteagry">
+        Makeup | Dresses For Girls | T-Shirts | Sandals Headphones | Babydolls |
+        Blazers For Men | Handbags | Ladies Watches | Bags | Sport Shoes |
+        Reebok Shoes | Puma Shoes | Boxers | Wallets |Tops | Designers Sarees
+      </p>
+      <hr />
+      <center className="que2">
+        © 2023 www.myntra.com (ClonedByDolly) All rights reserved.
+      </center>
+      <hr />
+    </div>
+  );
+};
+
+export default Footer2;
